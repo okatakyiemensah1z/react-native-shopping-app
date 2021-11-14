8a948a1c456183699b73bafd86dc0dabc5f72515
okatakyiemensah1z <micmensmensah511@gmail.com>
2021-11-15 06:41:46 +0800

    Updated some things...

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import HomeScreen from './screens/HomeScreen';
+import ProductDetails from './screens/ProductDetails';
+import Cart from './screens/Cart';
 
 const Main = createNativeStackNavigator();
 
@@ -12,12 +14,18 @@ export default function App() {
   return (
    <NavigationContainer>
      <Main.Navigator>
-       <Main.Screen 
+       {/* <Main.Screen 
        name='Login'
-       component={Login}/>
+       component={Login}/> */}
        <Main.Screen 
        name='Home'
        component={HomeScreen}/>
+       <Main.Screen
+       name='Details'
+       component={ProductDetails}/>
+       <Main.Screen
+       name='Cart'
+       component={Cart} />
      </Main.Navigator>
    </NavigationContainer>
   );
